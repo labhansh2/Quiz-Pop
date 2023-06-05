af829a76ff90ef85077ccd192f5dddee32a82bfb
labhansh2 <labhanshtimande4@gmail.com>
2023-06-05 14:02:07 +0530

    added setting for optimal deployement on surge

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,39 @@
-import { defineConfig } from 'vite';
-import { join } from 'path';
+import { defineConfig } from "vite";
+import { join } from "path";
 
 export default defineConfig({
-  root: join(process.cwd(), 'src'),
-  server: {
-    port: 3000,
-    open: '/index.html',
-  },
-  preview: {
-    port: 8080,
-  },
-  build: {
-    outDir: join(process.cwd(), 'dist'),
-  },
+    root: join(process.cwd(), "src"),
+    server: {
+        port: 3000,
+        open: "/index.html",
+    },
+    preview: {
+        port: 8080,
+    },
+    build: {
+        outDir: join(process.cwd(), "dist"),
+        rollupOptions: {
+            input: {
+                main: "src/index.html",
+                instruction: "src/quiz-instruction.html",
+                quiz: "src/quiz.html",
+                score: "src/score.html",
+            },
+        },
+    },
 });
+
+// import { defineConfig } from "vite";
+// import { join } from "path";
+
+// export default defineConfig({
+//     root: join(process.cwd(), "src"),
+//     build: {
+//         outDir: join(process.cwd(), "dist"),
+//         rollupOptions: {
+//             input: {
+//                 score: "src/score.html",
+//             },
+//         },
+//     },
+// });
